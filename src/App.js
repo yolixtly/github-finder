14106911ce8ed98b29740a3fff166919339a4c17
Yolixtly Anderson <yolixtly@Yolixtlys-MacBook-Pro-2.local>
2020-01-08 13:08:14 -0800

    added proptypes checks to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
@@ -10,16 +11,19 @@ class App extends React.Component {
     loading: false
   }
 
-  async componentDidMount() {
-    // HTTP Requests Go Here when we do not have Redux
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  }
+  searchUsers = async text => {
     this.setState({
       loading: true
     });
 
-    await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`).then(res => {
+    let url = `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    await axios.get(url).then(res => {
       this.setState({
         loading: false,
-        users: res.data
+        users: res.data.items
       });
     });
   }
@@ -28,7 +32,7 @@ class App extends React.Component {
       <div className="App">
         <Navbar title="Github Finder" icon='fab fa-github' />
         <div className="container ">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
