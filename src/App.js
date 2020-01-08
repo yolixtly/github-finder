dcde6c1e3c6a3dde50c2607ab464dcdc28d9f583
Yolixtly Anderson <yolixtly@Yolixtlys-MacBook-Pro-2.local>
2020-01-07 22:22:25 -0800

    first http call on App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    // HTTP Requests Go Here when we do not have Redux
+    this.setState({
+      loading: true
+    });
+
+    await axios.get('https://api.github.com/users').then(res => {
+      this.setState({  
+        loading: false,
+        users: res.data
+      });
+    });
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon='fab fa-github' />
         <div className="container ">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
