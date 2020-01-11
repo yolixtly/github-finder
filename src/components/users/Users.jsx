504f1e97a6d8378cfd193f669e3d76306aa96c51
yolixtly <yolixtly_13@hotmail.com>
2020-01-10 19:13:44 -0800

    Context implementation on component

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+    const githubContext = useContext(GithubContext);
+
+    const { users, loading } = githubContext;
     if (loading) return <Spinner />;
     return (
         <div style={userStyle}>
@@ -14,10 +17,6 @@ const Users = ({ users, loading }) => {
     );
 };
 
-Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool
-};
 const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
