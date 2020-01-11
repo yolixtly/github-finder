f0779ee128b1043949488080c5f3d3e2c1f965f0
yolixtly <yolixtly_13@hotmail.com>
2020-01-10 16:29:50 -0800

    Upgrade User Class to functional Component with useEffect for first Render

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,101 +1,101 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 
-class User extends Component {
-    componentDidMount() {
-        const { login } = this.props.match.params;
-        this.props.getUser(login);
-        this.props.getUserRepos(login);
+const User = ({
+    loading,
+    repos,
+    getUser,
+    getUserRepos,
+    match,
+    user: {
+        name,
+        avatar_url,
+        company,
+        location,
+        bio,
+        blog,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable
     }
+}) => {
+    useEffect(() => {
+        const { login } = match.params;
+        getUser(login);
+        getUserRepos(login);
+        // eslint-disable-next-line
+    }, []);
 
-    render() {
-        const {
-            loading,
-            repos,
-            user: {
-                name,
-                avatar_url,
-                company,
-                location,
-                bio,
-                blog,
-                login,
-                html_url,
-                followers,
-                following,
-                public_repos,
-                public_gists,
-                hireable
-            }
-        } = this.props;
-
-        if (loading) return <Spinner />;
-        return (
-            <Fragment>
-                <Link to='/' className='btn btn-light'>
-                    Back to Search
-                </Link>
-                Hireable:{' '}
-                {hireable ? (
-                    <i className='fas fa-check text-success' />
-                ) : (
-                    <i className='fas fa-times-circle text-danger' />
-                )}
-                <div className='card grid-2'>
-                    <div className='all-center'>
-                        <img src={avatar_url} className='round-img' style={style} alt='' />
-                        <h1>{name}</h1>
-                        <p>Location: {location}</p>
-                    </div>
-                    <div>
-                        {bio && (
-                            <Fragment>
-                                <h3>Bio</h3>
-                                <p>{bio}</p>
-                            </Fragment>
-                        )}
-                        <a href={html_url} className='btn btn-dark my-1'>
-                            Visit Github Profile
-                        </a>
-                        <ul>
-                            <li>
-                                {login && (
-                                    <Fragment>
-                                        <strong>Username:</strong> {login}
-                                    </Fragment>
-                                )}
-                            </li>
-                            <li>
-                                {company && (
-                                    <Fragment>
-                                        <strong>Company:</strong> {company}
-                                    </Fragment>
-                                )}
-                            </li>
-                            <li>
-                                {blog && (
-                                    <Fragment>
-                                        <strong>Website:</strong> {blog}
-                                    </Fragment>
-                                )}
-                            </li>
-                        </ul>
-                    </div>
+    if (loading) return <Spinner />;
+    return (
+        <Fragment>
+            <Link to='/' className='btn btn-light'>
+                Back to Search
+            </Link>
+            Hireable:{' '}
+            {hireable ? (
+                <i className='fas fa-check text-success' />
+            ) : (
+                <i className='fas fa-times-circle text-danger' />
+            )}
+            <div className='card grid-2'>
+                <div className='all-center'>
+                    <img src={avatar_url} className='round-img' style={style} alt='' />
+                    <h1>{name}</h1>
+                    <p>Location: {location}</p>
                 </div>
-                <div className='card text-center'>
-                    <div className='badge badge-primary'>Followers: {followers}</div>
-                    <div className='badge badge-success'>Following: {following}</div>
-                    <div className='badge badge-danger'>Public Repos: {public_repos}</div>
-                    <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+                <div>
+                    {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                    )}
+                    <a href={html_url} className='btn btn-dark my-1'>
+                        Visit Github Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && (
+                                <Fragment>
+                                    <strong>Username:</strong> {login}
+                                </Fragment>
+                            )}
+                        </li>
+                        <li>
+                            {company && (
+                                <Fragment>
+                                    <strong>Company:</strong> {company}
+                                </Fragment>
+                            )}
+                        </li>
+                        <li>
+                            {blog && (
+                                <Fragment>
+                                    <strong>Website:</strong> {blog}
+                                </Fragment>
+                            )}
+                        </li>
+                    </ul>
                 </div>
-                <Repos repos={repos} />
-            </Fragment>
-        );
-    }
-}
+            </div>
+            <div className='card text-center'>
+                <div className='badge badge-primary'>Followers: {followers}</div>
+                <div className='badge badge-success'>Following: {following}</div>
+                <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+                <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+            </div>
+            <Repos repos={repos} />
+        </Fragment>
+    );
+};
 
 const style = {
     width: '150px'
