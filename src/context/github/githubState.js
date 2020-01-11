5e2bf79896a525ce70431dec3b710b23b097c22b
yolixtly <yolixtly_13@hotmail.com>
2020-01-10 20:12:45 -0800

    config for Netlify Deployment

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -12,6 +12,17 @@ import {
 } from '../types';
 // Has the initial State and Actions
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV != 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
     // Global State for any Github related data
     const initialState = {
@@ -29,8 +40,8 @@ const GithubState = props => {
 
         let url = 'https://api.github.com/search/users?' +
             `q=${text}&` +
-            `client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&` +
-            `client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+            `client_id=${githubClientId}&` +
+            `client_secret=${githubClientSecret}`;
 
         await axios.get(url).then(res => {
             dispatch({
@@ -45,8 +56,8 @@ const GithubState = props => {
         setLoading(true);
 
         let url = `https://api.github.com/users/${username}?` +
-            `client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&` +
-            `client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+            `client_id=${githubClientId}&` +
+            `client_secret=${githubClientSecret}`;
 
         await axios.get(url).then(res => {
             dispatch({
@@ -62,8 +73,8 @@ const GithubState = props => {
 
         let url = `https://api.github.com/users/${username}/repos?` +
             `per_page=5&sort=created:asc&` +
-            `client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&` +
-            `client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+            `client_id=${githubClientId}&` +
+            `client_secret=${githubClientSecret}`;
 
         await axios.get(url).then(res => {
             dispatch({
