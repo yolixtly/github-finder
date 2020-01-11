d0c5b1f27d1df68c5565a24e3fe49ee86ca1a295
yolixtly <yolixtly_13@hotmail.com>
2020-01-10 19:12:51 -0800

    included Reducer

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,42 @@
+// Decides how to dispatch a given action from the app to the state
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_REPOS,
+    GET_USER
+} from '../types';
+
+export default (state, action) => {
+    switch (action.type) {
+        case GET_USER: {
+            return {
+                ...state,
+                user: action.payload,
+                loading: false
+            }
+        }
+        case SEARCH_USERS: {
+            return {
+                ...state,
+                users: action.payload,
+                loading: false
+            }
+        }
+        case CLEAR_USERS: {
+            return {
+                ...state,
+                users: [],
+                loading: false
+            }
+        }
+        case SET_LOADING: {
+            return {
+                ...state,
+                loading: true
+            }
+        }
+        default:
+            return state;
+    }
+}
