d05ffc2ca436d8cccb3d1ce9204f4f1898dd30a5
yolixtly <yolixtly_13@hotmail.com>
2020-01-10 19:13:30 -0800

    Context implementation on User file

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,31 +1,30 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({
-    loading,
-    repos,
-    getUser,
-    getUserRepos,
-    match,
-    user: {
-        name,
-        avatar_url,
-        company,
-        location,
-        bio,
-        blog,
-        login,
-        html_url,
-        followers,
-        following,
-        public_repos,
-        public_gists,
-        hireable
-    }
-}) => {
+const User = ({ loading, repos, getUserRepos, match }) => {
+    const githubContext = useContext(GithubContext);
+    const {
+        getUser,
+        user: {
+            name,
+            avatar_url,
+            company,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable
+        }
+    } = githubContext;
     useEffect(() => {
         const { login } = match.params;
         getUser(login);
@@ -104,7 +103,6 @@ const style = {
 User.proptTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
     repos: PropTypes.array.isRequired,
     getUserRepos: PropTypes.func.isRequired
 };
